2152069adc3c3bbac996a1de33ad0f3dd7987845
piyushk <piyushk@gmail.com>
2013-12-18 22:34:27 +0530

    added corrected mark_doors and mark_locations

--- bwi_planning_common/include/bwi_planning_common/structures.h
+++ bwi_planning_common/include/bwi_planning_common/structures.h
@@ -1,14 +1,12 @@
 #ifndef STRUCTURES_9TKSKPHM
 #define STRUCTURES_9TKSKPHM
 
-#include <opencv/cv.h>
+#include <bwi_tools/point.h>
 #include <stdint.h>
 #include <string>
 #include <yaml-cpp/yaml.h>
 
 namespace bwi_planning_common {
-
-  typedef cv::Point2f Point2f;
 
   void readLocationFile(const std::string& filename, 
       std::vector<std::string>& locations, std::vector<int32_t>& location_map);
@@ -17,7 +15,7 @@ namespace bwi_planning_common {
     public:
       std::string name;
       std::string approach_names[2];
-      Point2f approach_points[2];
+      bwi::Point2f approach_points[2];
       float approach_yaw[2];
       float width;
   };
@@ -25,6 +23,7 @@ namespace bwi_planning_common {
   void readDoorFile(const std::string& filename, std::vector<Door>& doors);
 
   const size_t NO_DOOR_IDX = (size_t) -1; 
+  const int INVALID_LOCATION = -1;
 
 } /* bwi_planning_common */
 
